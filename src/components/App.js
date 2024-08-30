f337fb6477c1a853efbafd949fed5680b7e0e53a
soumyabrata <soumyabratam6@gmail.com>
2024-08-30 16:18:57 +0530

    add file

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,60 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Login from './components/Login';
-import Home from './components/Home';
-import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Route, Routes, Link, Navigate } from 'react-router-dom';
 
-const PrivateRoute = ({ isAuth, children }) => {
-  return isAuth ? children : <Navigate to="/" />;
-};
+const App = () => {
+  const [authenticated, setAuthenticated] = useState(false);
 
-function App() {
-  const [isAuth, setIsAuth] = useState(false);
+  const handleLoginLogout = () => {
+    setAuthenticated(!authenticated);
+  };
 
   return (
     <Router>
-      <Navbar isAuth={isAuth} />
-      <Routes>
-        <Route path="/" element={<Login setAuth={setIsAuth} />} />
-        <Route
-          path="/home"
-          element={
-            <PrivateRoute isAuth={isAuth}>
-              <Home />
-            </PrivateRoute>
-          }
-        />
-      </Routes>
+      <div className="main-container">
+        <ul>
+        {authenticated ? 'Logged in, Now you can enter Playground' : 'You are not authenticated, Please login first'}
+          <li>
+            <Link to="/playground">PlayGround</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        </ul>
+        <button onClick={handleLoginLogout}>
+          {authenticated ? 'Log Out' : 'Log In'}
+        </button>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              authenticated ? (
+                <Navigate to="/playground" />
+              ) : (
+                <Navigate to="/login" />
+              )
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              authenticated ? (
+                <Navigate to="/playground" />
+              ) : (
+                <div>
+                  <p>Login</p>
+                </div>
+              )
+            }
+          />
+          <Route
+            path="/playground"
+            element={authenticated ? <div>Hi Welcome to Code PlayGround</div> : <Navigate to="/login" />}
+          />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </div>
     </Router>
   );
-}
+};
 
 export default App;
